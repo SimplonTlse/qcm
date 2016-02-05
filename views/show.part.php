5d4d9f8e0cfc62ea84c1f557a3acbfe4e5459004
Amine EL G <aelghayate@simplon.co>
2016-02-05 17:20:36 +0100

    Update show.part.php

--- views/show.part.php
+++ views/show.part.php
@@ -1,6 +1,9 @@
-
+<?php require BASE . 'views/header.part.php'; ?>
 
 <h1>yatta</h1>
+
+<!-- on fait une boucle sur les reponses traitées, 
+la version de foreach qui finit par deux points est plus commode dans le contexte d'une page html -->
 
 <?php foreach(checkAnswers() as $key => $p) : ?>
 
@@ -12,3 +15,5 @@
 	<?php endif; ?>
 
 <?php endforeach; ?>
+
+<?php require BASE . 'views/footer.part.php'; ?>
