0a8143b52aeff35282654bba141ccacbdb46a897
Amine EL G <aelghayate@simplon.co>
2016-02-05 17:23:06 +0100

    Update header.part.php

--- views/header.part.php
+++ views/header.part.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.css">
 	<title>Da QCM</title>
 </head>
 <body>
